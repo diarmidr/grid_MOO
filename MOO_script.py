4c1153c50066b73c4863a58e6cee5322ddc61568
diarmidr <d.roberts1@sheffield.ac.uk>
2023-11-10 11:24:25 +0000

    added cutting plane total_gen_cap > max demand

--- MOO_script.py
+++ MOO_script.py
@@ -16,15 +16,13 @@ t_data = pd.read_csv("data/generator_profiles.csv")
 ###########################
 demand_scaling = 1  # I'm fixing demand at current levels, no point scaling it as it will also change shape
 # Variable key, showing order for matching cost/performance params to the variable array:
-# P_nuclear, P_PV, P_wind_offshore, P_ESS_1, P_ESS_2, P_ESS_3, P_ESS_4, dur_ESS_1, dur_ESS_2, dur_ESS_3, dur_ESS_4, P_OCGT
 # Ones that are not dispatchable, just get added to the supply/deficit balance automatically
 # idx_0: P_nuclear, idx_1: P_PV, idx_2: P_wind_offshore, idx_3: P_BECCS
 # Dispatchable generation variables, in order of dispatch (most efficient storage through to most emitting back-up)
 # idx_4: P_ESS_1, idx_5: P_ESS_2, idx_6: P_ESS_3, idx_7: P_ESS_4, idx_8: P_OCGT
 # Storage duration variables:
 # idx_9: dur_ESS_1, idx_10: dur_ESS_2, idx_11: dur_ESS_3, idx_12: dur_ESS_4
-# xl = np.array([100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-# xu = np.array([100, 100, 200, 10, 100, 10, 100, 100, 55, 1000, 1000, 1000, 1000]
+
 class GridMOOProblem(ElementwiseProblem):
 
     def __init__(self, **kwargs):
@@ -32,7 +30,7 @@ class GridMOOProblem(ElementwiseProblem):
                          n_obj=4,
                          n_ieq_constr=2,
                          xl=np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-                         xu=np.array([100, 100, 200, 5, 100, 10, 100, 100, 55, 1000, 1000, 1000, 1000]), **kwargs)
+                         xu=np.array([100, 100, 200, 5, 100, 10, 100, 100, 55, 24, 1000, 1000, 1000]), **kwargs)
 
     #return fuel_cost, percentage_demand_met, co2_emissions
 
@@ -45,6 +43,8 @@ class GridMOOProblem(ElementwiseProblem):
         # Constraints, in format < 0
         g1 = x[5] * x[10] - 700  # PHS Energy rating less than 700Gwh (with power < 10GW set in bounds above)
         g2 = x[4] + x[5] + x[6] + x[7] - 100  # Total storage power < 100GW
+        #  Total generation > peak demand
+        g3 = x[0] - x[1] - x[2] - x[3] - x[4] - x[5] - x[6] - x[7] - x[8] - x[9] + max(t_data["demand"])/1000
         print("Objectives", -f1, -f2, f3, f4)
         out["F"] = [f1, f2, f3, f4]
         out["G"] = [g1, g2]
@@ -52,11 +52,11 @@ class GridMOOProblem(ElementwiseProblem):
 
 problem = GridMOOProblem()
 
-algorithm = NSGA2(pop_size=10)
+algorithm = NSGA2(pop_size=200)
 
 res = minimize(problem,
                algorithm,
-               ("n_gen", 5),
+               ("n_gen", 100),
                verbose=True,
                return_least_infeasible=True)
 print("execution time", res.exec_time)
